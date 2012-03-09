a2eeef0008a91590a4d605b0545ff87a5d3c7cf7
Tom Hauser <tom.hauser@gmail.com>
2012-03-08 19:31:30 -0500

    adding constants class

---
+++ SpaceGame/src/space/entities/Constants.java
@@ -0,0 +1,10 @@
+package space.entities;
+
+public class Constants {
+	public static final float PLAYER_MOVE_SPEED = 3f;
+	public static final float PLAYER_TURN_SPEED = .25f;
+	public static final float PLAYER_STRAFE_SPEED = 4f;
+	public static final float PLAYER_MAX_SPEED = 25f;
+	public static final float CAM_ZOOM = .08f;
+	public static final float DECAY_SPEED = .010f;
+}
