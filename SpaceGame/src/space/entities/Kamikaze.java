2c8b231e2b3ae505c4ee370f3565fcaa7a5799a7
Tom Hauser <tom.hauser@gmail.com>
2012-03-18 16:21:38 -0400

    Kamikaze type of enemy preliminary class

---
+++ SpaceGame/src/space/entities/Kamikaze.java
@@ -0,0 +1,43 @@
+package space.entities;
+
+import org.newdawn.slick.Image;
+
+import space.GameContext;
+
+public class Kamikaze extends Enemy {
+	
+	// a Kamikaze enemy
+
+	public Kamikaze(int wave){
+		setHealth(100 + (wave*10));
+		setDamage(80 + (wave*20)); // quickly become very powerful
+		try{
+			setImage(new Image("res/kamikaze.png"));
+		}catch (Exception e){}
+		setBody(createBody());		
+		body.setMaxVelocity(Constants.ENEMY_KAMIKAZE_SPEED, Constants.ENEMY_KAMIKAZE_SPEED);
+		enemyWidth = enemyImage.getWidth()/2;
+		enemyHeight = enemyImage.getHeight()/2;
+	}
+
+	public void update(GameContext context, int delta){
+		// move towards the player relentlessly
+		// somehow give the enemies a reference to the player, or a reference to the InGameState
+		Ship player = context.getInGameState().getPlayer();
+		// kamikazes rotate to face the player
+		setHeading(player.getX(), player.getY());
+		thrust(delta);
+	}
+
+	@Override
+	public void collide(Entity other) {
+		if (other instanceof Bullet){
+			Bullet bullet = (Bullet)other;
+			takeDamage(bullet.getDamage());
+			bullet.kill();
+		}
+		else if (other instanceof Ship){
+			kill();
+		}
+	}
+}
