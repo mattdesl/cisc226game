ebf41c1f5a348c873ebb1c31cb2374b01839b70b
Tom Hauser <tom.hauser@gmail.com>
2012-03-18 17:57:46 -0400

    speed of different entities modified

--- SpaceGame/src/space/entities/Constants.java
+++ SpaceGame/src/space/entities/Constants.java
@@ -1,11 +1,11 @@
 package space.entities;
 
 public class Constants {
-	public static final float PLAYER_MOVE_SPEED = 1000f;
+	public static final float PLAYER_MOVE_SPEED = 1500f;
 	public static final float PLAYER_TURN_SPEED = 0.25f;
 	public static final float PLAYER_STRAFE_SPEED = 1200f;
 	public static final float PLAYER_MAX_SPEED = 40f;
-	public static final float PLAYER_BLASTER_SPEED = 1000f;
+	public static final float PLAYER_BLASTER_SPEED = 2000f;
 	public static final float CAM_ZOOM = .08f;
 	public static final float DECAY_SPEED = .010f;
 
