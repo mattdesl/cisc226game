5d7b7f815e4f107eca49e6a9b064b52fde4470c9
Tom Hauser <tom.hauser@gmail.com>
2012-03-20 02:24:57 -0400

    add different thrust types for Wingbat. modify death condition. remove deprecated methods

--- SpaceGame/src/space/entities/Enemy.java
+++ SpaceGame/src/space/entities/Enemy.java
@@ -27,10 +27,13 @@ public abstract class Enemy extends AbstractEntity {
 	// enemy strength based upon wave
 	public Enemy(Image image) {
 		this.enemyImage = image;
+		enemyWidth = enemyImage.getWidth()/2;
+		enemyHeight = enemyImage.getHeight()/2;
 	}
 	
 	public Body createBody(){
 		Body body = new Body(new Circle(enemyImage.getWidth()/2f), 10f);
+		body.addBit(Constants.BIT_ENEMY);
 		body.setUserData(this);
 		return body;
 	}
@@ -88,12 +91,32 @@ public abstract class Enemy extends AbstractEntity {
 		setRotation((float)Math.toDegrees(r));
 	}
 	
+	// thrust straight ahead	
 	public void thrust(int delta){
 		float dirXAmt = dirX * delta * Constants.ENEMY_KAMIKAZE_SPEED;
 		float dirYAmt = dirY * delta * Constants.ENEMY_KAMIKAZE_SPEED;
 		addForce(dirXAmt, dirYAmt);
 	}
-
+	
+	//reverse thrust, to keep the enemy at a more constant distance
+	public void thrustReverse(int delta){
+		float dirXAmt = -(dirX * delta * Constants.ENEMY_WINGBAT_REV_SPEED);
+		float dirYAmt = -(dirY * delta * Constants.ENEMY_WINGBAT_REV_SPEED);
+		addForce(dirXAmt, dirYAmt);
+	}
+	
+	// thrust sideways. the boolean indicates the heading, false for right, true for left
+	public void thrustSide(int delta, boolean left){
+		double r;
+		if (left){
+			r = angle - Math.PI/2;
+		} else {
+			r = angle + Math.PI/2;
+		}
+		float dirX = (float)Math.sin(r);
+		float dirY= (float)-Math.cos(r);
+		addForce(dirX * delta * Constants.ENEMY_WINGBAT_SPEED, dirY * delta * Constants.ENEMY_WINGBAT_SPEED);
+	}
 	public void draw(GameContext context, SpriteBatch batch, Graphics g){
 		float newX = getX() - enemyWidth;
 		float newY = getY() - enemyWidth;
@@ -104,7 +127,7 @@ public abstract class Enemy extends AbstractEntity {
 	
 	public void takeDamage(int damage){
 		int newHealth = getHealth() - damage;
-		if (newHealth < 0){ // dead
+		if (newHealth <= 0){ // dead
 			setHealth(0);
 			kill();
 			System.out.println("Enemy destroyed");
@@ -114,15 +137,8 @@ public abstract class Enemy extends AbstractEntity {
 		}
 	}
 	
-	public void ensureWithinBounds(int width, int height) {
-		if (getX() < -getWidth()) {
-			setPosition(width, getY());
-		} else if (getX() > width+getWidth())
-			setPosition(-getWidth(), getY());
-		
-		if (getY() < -getHeight()) 
-			setPosition(getX(), height);
-		else if (getY() > height+getHeight())
-			setPosition(getX(), -getHeight());
+	// we play the death animation
+	public void playDeath(){
+		kill(); 		
 	}
 }
