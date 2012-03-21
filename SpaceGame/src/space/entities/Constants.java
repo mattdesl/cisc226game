ccd8910d325c6728063809d8712de2e1f30bc95c
Tom Hauser <tom.hauser@gmail.com>
2012-03-20 22:42:00 -0400

    Adding more constants

--- SpaceGame/src/space/entities/Constants.java
+++ SpaceGame/src/space/entities/Constants.java
@@ -6,6 +6,8 @@ public class Constants {
 	public static final float PLAYER_STRAFE_SPEED = 1200f;
 	public static final float PLAYER_MAX_SPEED = 40f;
 	public static final float PLAYER_BLASTER_SPEED = 2000f;
+	public static final int PLAYER_SHIELD_REGEN_SPEED = 2000;
+	public static final int PLAYER_SHOOTING_COOLDOWN = 200;
 	public static final float CAM_ZOOM = .08f;
 	public static final float DECAY_SPEED = .010f;
 
@@ -20,5 +22,8 @@ public class Constants {
 	public static final float ENEMY_KAMIKAZE_SPEED = 30f; 
 	public static final float ENEMY_WINGBAT_SPEED = 50f; // can catch the player np
 	public static final float ENEMY_WINGBAT_REV_SPEED = 10f; // reverse speed 
+	public static final int ENEMY_WINGBAT_SHOOTING_COOLDOWN = 1550; // initial wingbat shooting speed
 	public static final float ENEMY_BLASTER_SPEED = 1200f;
+	
+	public static final int WAVE_REST_TIME = 2500; // the amount of time in ms before the next wave spawns
 }
